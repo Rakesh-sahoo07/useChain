61c6795cb4482f31d74b79b9c3105b479023e3b8
Rakesh-sahoo7 <rakesh.sahoo@squbix.com>
2024-03-08 00:13:21 +0530

    Refactor useChain hook to support different login types

--- useChain.js
+++ useChain.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ethers } from 'ethers';
 
-const useChain = (contractAddress, contractABI) => {
+const useChain = (contractAddress, contractABI, loginType = 'onRender') => {
   const [state, setState] = useState({
     provider: null,
     signer: null,
@@ -9,30 +9,52 @@ const useChain = (contractAddress, contractABI) => {
   });
   const [account, setAccount] = useState("");
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const { ethereum } = window;
+  const loginWithUseEffect = async () => {
+    try {
+      const { ethereum } = window;
 
-        if (ethereum) {
-          const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const signer = provider.getSigner();
-          const contract = new ethers.Contract(contractAddress, contractABI, signer);
-
-          const userAccount = accounts[0];
-          setAccount(userAccount);
-          setState({ provider, signer, contract });
-        } else {
-          throw new Error("Please install MetaMask");
-        }
-      } catch (error) {
-        console.error(error);
+      if (ethereum) {
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+        const userAccount = accounts[0];
+        setAccount(userAccount);
+        setState({ provider, signer, contract });
+      } else {
+        throw new Error("Please install MetaMask");
       }
-    };
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    init();
-  }, [contractAddress, contractABI]);
+  const loginWithAsyncAwait = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+        const userAccount = accounts[0];
+        setAccount(userAccount);
+        setState({ provider, signer, contract });
+      } else {
+        throw new Error("Please install MetaMask");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  if (loginType === 'onRender') {
+    useEffect(() => {
+      loginWithUseEffect();
+    }, [contractAddress, contractABI]);
+  } else if (loginType === 'onButtonClick') {
+    loginWithAsyncAwait();
+  }
 
   return { state, account };
 };
